8c2dc8d32655f21943bf7b5c5b94f186e8097fca
ernaneaugusto <ernaneatcipriano@gmail.com>
2020-10-12 21:02:27 -0300

    criado modulo pagina Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./views/home/home.module').then(home => home.HomeModule)
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
